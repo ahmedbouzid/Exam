d150825f113009b87bac34a4a66fcd7374aee362
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-01 11:41:27 +0200

    create auth Service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,10 @@
+import { HttpBackend } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http : HttpBackend) { }
+}
