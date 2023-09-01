d5cf55ff6318b50641cfce3c06de40fc53d26012
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-01 14:34:11 +0200

    - create service for registre -dependency injection

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,27 +1,63 @@
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  userForm  !: FormGroup ;
-  constructor( private formBuilder : FormBuilder) {}
+  userForm!: FormGroup;
+  students: any[] = [];
 
-    ngOnInit(): void {
-      throw new Error('Method not implemented.');
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.CreateForm();
+    this.getAllStudents();
+  }
+
+  CreateForm() {
+    this.userForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+    });
+  }
+
+  formSubmission() {
+    const model = {
+      username: this.userForm.value.username,
+      email: this.userForm.value.email,
+      password: this.userForm.value.password,
+    };
+
+    // Check if the email already exists in the students array
+    const emailExists = this.students.some((student) => student.email === model.email);
+
+    if (emailExists) {
+      this.toastr.error('Email already exists');
+    } else {
+      this.service.createUser(model).subscribe((res) => {
+        this.toastr.success('Account created successfully');
+        this.router.navigate(['subjects']);
+      });
     }
+  }
 
-  CreateForm( ){
-    this.userForm = this.formBuilder.group ({
-      username : ['' , Validators.required] ,
-      email : ['' , Validators.required] ,
-      password : ['' , Validators.required] ,
-      confirmPassword : ['' , Validators.required]
-
-
-    })
+  getAllStudents() {
+    this.service.getStudents().subscribe((students: any) => {
+      this.students = students;
+      console.log('Students:', this.students);
+    });
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,10 +1,20 @@
-import { HttpBackend } from '@angular/common/http';
+import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http : HttpBackend) { }
+  constructor(private http : HttpClient) { }
+
+  APi = 'http://localhost:3000/'
+  createUser(model : any) {
+    return this.http.post( this.APi+'students' , model)
+  }
+  getStudents () {
+    return this.http.get(this.APi+'students')
+  }
 }
