25382701f5486e7368a5b91662bca5124a118d0b
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-01 11:42:33 +0200

    registre logic

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  userForm  !: FormGroup ;
+  constructor( private formBuilder : FormBuilder) {}
 
+    ngOnInit(): void {
+      throw new Error('Method not implemented.');
+    }
+
+  CreateForm( ){
+    this.userForm = this.formBuilder.group ({
+      username : ['' , Validators.required] ,
+      email : ['' , Validators.required] ,
+      password : ['' , Validators.required] ,
+      confirmPassword : ['' , Validators.required]
+
+
+    })
+  }
 }
